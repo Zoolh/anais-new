ea7108d5901471182934e0ff19ced98e76009a8f
Nicolas Palluel <nicolas.palluel@lilo.org>
2021-01-11 16:45:18 +0100

    connection with node + first step into template

---
+++ anais-front-v2/src/components/Pictures/ProfilPicture.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+// @material-ui/core components
+import { makeStyles } from "@material-ui/core/styles";
+
+import typographyStyle from "assets/jss/material-kit-pro-react/views/componentsSections/typographyStyle.js";
+
+// Images
+import portraitAnais from 'assets/img/landing-page/anais-portrait-resize.jpg';
+
+const useStyles = makeStyles(typographyStyle);
+
+export default function ProfilPicture() {
+  const classes = useStyles();
+  return (
+    <div className={classes.section}>
+      <div className={classes.container}>
+        <div id="images">
+              <img
+                src={portraitAnais}
+                alt="..."
+                className={
+                  classes.imgRaised +
+                  " " +
+                  classes.imgRoundedCircle +
+                  " " +
+                  classes.imgFluid
+                }
+              />
+        </div>
+        <div className={classes.space50} />
+      </div>
+    </div>
+  );
+}
---
+++ anais-front-v2/src/views/LandingPage/LandingPage.js
@@ -0,0 +1,104 @@
+/*eslint-disable*/ import React from "react";
+// nodejs library to set properties for components
+import PropTypes from "prop-types";
+// nodejs library that concatenates classes
+import classNames from "classnames";
+// @material-ui/core components
+import { makeStyles } from "@material-ui/core/styles";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+
+// core components
+import Header from "components/Header/Header.js";
+import Footer from "components/Footer/Footer.js";
+import GridContainer from "components/Grid/GridContainer.js";
+import GridItem from "components/Grid/GridItem.js";
+import HeaderLinks from "components/Header/HeaderLinks.js";
+import Parallax from "components/Parallax/Parallax.js";
+
+import ProfilPicture from "components/Pictures/ProfilPicture.js";
+
+import landingPageStyle from "assets/jss/material-kit-pro-react/views/landingPageStyle.js";
+
+// Sections for this page
+import SectionProduct from "./Sections/SectionProduct.js";
+import SectionTeam from "./Sections/SectionTeam.js";
+import SectionWork from "./Sections/SectionWork.js";
+
+const useStyles = makeStyles(landingPageStyle);
+
+export default function LandingPage({ ...rest }) {
+  React.useEffect(() => {
+    window.scrollTo(0, 0);
+    document.body.scrollTop = 0;
+  });
+  const classes = useStyles();
+  return (
+    <div>
+      <Header
+        color="transparent"
+        // brand="Material Kit PRO React"
+        links={<HeaderLinks dropdownHoverColor="info" />}
+        fixed
+        changeColorOnScroll={{
+          height: 300,
+          color: "info"
+        }}
+        {...rest}
+      />
+      <Parallax backgroundColor="#fee4de" color="black">
+        <div className={classes.container}>
+
+          <GridContainer color="black">
+            <GridItem xs={4} sm={6} md={6}>
+              <h1 className={classes.title}>ANAïS L.</h1>
+              <h4>
+                Chaque femme est différente et a sa propre singularité...
+                Mon objectif est de vous aider à trouver votre équilibre...
+                La mode peut aussi être un outil de bienêtre.
+              </h4>
+              <br />
+            </GridItem>
+            <GridItem xs={4} sm={6} md={6}>
+              <ProfilPicture />
+            </GridItem>
+          </GridContainer>
+        </div>
+      </Parallax>
+      <div className={classNames(classes.main, classes.mainRaised)}>
+        <div className={classes.container}>
+          <SectionProduct />
+          <SectionTeam />
+          <SectionWork />
+        </div>
+      </div>
+      <Footer
+        content={
+          <div>
+            <div className={classes.left}>
+              <List className={classes.list}>
+                <ListItem className={classes.inlineBlock}>
+                  <a
+                    href="https://www.creative-tim.com/presentation?ref=mkpr-landing"
+                    target="_blank"
+                    className={classes.block}
+                  >
+                    A propos
+                  </a>
+                </ListItem>
+                <ListItem className={classes.inlineBlock}>
+                  <a href="//blog.creative-tim.com/" className={classes.block}>
+                    Blog
+                  </a>
+                </ListItem>
+              </List>
+            </div>
+            <div className={classes.right}>
+              &copy; Nicolas
+            </div>
+          </div>
+        }
+      />
+    </div>
+  );
+}
